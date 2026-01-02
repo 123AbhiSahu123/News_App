4245c4056c2e37257e2f1d09dc5e48b63c4d8faa
123AbhiSahu123 <sahuabhishek3810@gmail.com>
2026-01-02 21:43:31 +0530

    changs

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,26 +6,26 @@ export default class Footer extends Component {
     return (
       <>
         <footer>
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>Politics</h4>
           </div>
 
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>Education</h4>
           </div>
 
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>Bussiness</h4>
           </div>
 
 
-          <div class="footer-col">
+          <div className="footer-col">
             <h4>Follow us</h4>
-            <div class="links">
-              <a href="https://www.facebook.com/shivamsahu1577"><i class="fab fa-facebook-f"></i></a>
-              <a href="https://x.com/Abhishe62526101"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-              <a href="https://www.linkedin.com/in/abhishek-sahu-a5a3472a4/"><i class="fab fa-linkedin-in"></i></a>
+            <div className="links">
+              <a href="https://www.facebook.com/shivamsahu1577"><i className="fab fa-facebook-f"></i></a>
+              <a href="https://x.com/Abhishe62526101"><i className="fab fa-twitter"></i></a>
+              <a href="#"><i className="fab fa-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/abhishek-sahu-a5a3472a4/"><i className="fab fa-linkedin-in"></i></a>
             </div>
           </div>
         </footer>
